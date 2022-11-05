463d7b929aba759df0cc37b711269fffe9fbd8ac
Krzysztof Chojnacki <krzysztof.michal.chojnacki@gmail.com>
2022-11-05 19:25:35 +0100

    2022-11-05

---
+++ notebooks/task_scheduler.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  5 18:49:19 2022
+
+@author: krzys
+"""
+
+def import_data():
+    pass
+
+def portion_task_data():
+    pass
+
+def portion_calendar():
+    pass
+
+def sort_data():
+    pass
+
+def define_start_date():
+    #per zadanie, jesli juz w toku harmonogramowania przypisalem mu date
+    #to usuwam je z pamieci dla oszczednosci ramu
+    pass
+
+def define_end_date():
+    #per zadanie, jesli juz w toku harmonogramowania przypisalem mu date
+    #to usuwam je z pamieci dla oszczednosci ramu
+    pass
+
+def define_if_risk():
+    pass
